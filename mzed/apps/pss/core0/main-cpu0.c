fcbe37be0dc815faaa4af4e866c234e00a344f2c
john dawson <john.f.dawson20@gmail.com>
2015-07-28 06:31:19 -0400

    added simple amp code to pss directory

--- mzed/apps/pss/core0/main-cpu0.c
+++ mzed/apps/pss/core0/main-cpu0.c
@@ -1,7 +1,44 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include "xil_io.h"
+#include "xil_mmu.h"
+#include "xil_exception.h"
+#include "xpseudo_asm.h"
+#include "xscugic.h"
+
+#define sev() __asm__("sev")
+#define CPU1STARTADR 0xfffffff0
+#define semaphore  (*(volatile unsigned long *)(0xFFFF0000))
 
 int main()
 {
+	int Status;
 
+    //Disable cache on (upper 64k) of OCM
+    //S=b1 TEX=b100 AP=b11, Domain=b1111, C=b0, B=b0
+    Xil_SetTlbAttributes(0xFFFF0000,0x14de2);           
+
+    //init semaphore flag to 0
+    semaphore = 0;
+
+    print("CPU0: writing startaddress for cpu1\n\r");
+
+    Xil_Out32(CPU1STARTADR, 0x1FF80000);
+
+    dmb(); // data memory sync, waits until write has finished
+
+    print("CPU0: sending the SEV to wake up CPU1\n\r");
+    //send multicore event signal to wake core 1
+    sev();
+
+    while(1){
+
+		print("CPU0: Hello World CPU 0\n\r");
+		semaphore = 1;
+        
+        //block on semaphore lock 
+		while(semaphore == 1);
+
+    }
 	return(0);
 }
--- mzed/apps/pss/core1/main-cpu1.c
+++ mzed/apps/pss/core1/main-cpu1.c
@@ -1,7 +1,34 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include "xparameters.h"
+#include "xil_io.h"
+#include "xil_mmu.h"
+#include "xil_cache.h"
+#include "xil_exception.h"
+#include "xscugic.h"
+#include "sleep.h"
+
+
+#define semaphore  (*(volatile unsigned long *)(0xFFFF0000))
 
 int main()
 {
+	int Status;
+
+
+    //Disable cache on OCM
+    // S=b1 TEX=b100 AP=b11, Domain=b1111, C=b0, B=b0
+	Xil_SetTlbAttributes(0xFFFF0000,0x14de2);           
+
+
+    print("CPU1: init_platform\n\r");
+
+     while(1){
+     	while(semaphore == 0){};
+     	print("CPU1: Hello World CPU 1\n\r");
+     	semaphore = 0;
+
+     }
 
 	return(0);
 }
