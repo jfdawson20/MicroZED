33b947d07f51ca1d8783545aea547a9b2bea8701
John Dawson <john.f.dawson20@gmail.com>
2015-08-04 23:05:00 -0400

    final update for the night

--- mzed/apps/pss/common/bufferManager.c
+++ mzed/apps/pss/common/bufferManager.c
@@ -17,12 +17,13 @@
  * Detailed explanation.
  */
 
-struct packetDescriptorNode* CreateDescriptorList(uint32_t size)
+struct packetDescriptorNode* CreateDescriptorList(uint32_t size,uint32_t buffersize)
 {
 	/* variable decs */
         struct packetDescriptorNode* temp; 
         struct packetDescriptorNode* prev; 
         struct packetDescriptorNode* head; 
+	uint8_t* frameBuffer;
 	uint32_t* packetDescriptorList;
 	uint32_t i,descriptorIndex;
 	
@@ -43,16 +44,29 @@ struct packetDescriptorNode* CreateDescriptorList(uint32_t size)
 	{	
 		/* create temp node */
 		temp = (struct packetDescriptorNode*)malloc(sizeof(struct packetDescriptorNode));
+
+		/* create frame buffer*/
+		frameBuffer = (uint8_t*)malloc(sizeof(uint8_t)*buffersize*size);
 		
 		/* update previous nodes next field */
 		prev->next     		= temp;
 
 		/* update new node fields*/
-		temp->previous 		= prev;
-		temp->next              = head;
-		temp->id       		= i;
-		temp->packetDescriptor 	= packetDescriptorList + descriptorIndex;
+		temp->previous 		  = prev;
+		temp->next                = head;
+		temp->id       		  = i;
+		temp->packetDescriptor 	  = packetDescriptorList + descriptorIndex;
 		
+		/* set first word of packetDescriptor to base address of frame buffer */
+		/* if the descriptor has the last frame buffer, set bit 1 to 1 (wrap field)*/
+		if(i == (size-1))
+		{	
+			(*temp->packetDescriptor) = (frameBuffer | 0x00000002);
+		}
+		else
+		{
+			(*temp->packetDescriptor) = frameBuffer;
+		}
 		/* update previous pointer and descriptor Index */
 		prev 			= temp;
 		descriptorIndex 	= descriptorIndex + 2; 
@@ -62,4 +76,15 @@ struct packetDescriptorNode* CreateDescriptorList(uint32_t size)
 	return(head);
 }
 
-
+struct packetDescriptorNode* FreeBufferResources(struct packetDescriptorNode* buffer,uint32_t listSize, uint32_t bufferSize)
+{
+	uint32_t i;
+	for(i=0;i<listSize;i++)
+	{
+		for(i=0;i<bufferSize;i++)
+		{
+			free(packetDescriptorMode->
+		}
+	}	
+}
+}
